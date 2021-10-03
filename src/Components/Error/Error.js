8812c99afb05e390c11808de1c18c2f9daa3b1af
AfianAvro <wahedemon09@gmail.com>
2021-10-03 14:14:06 +0600

    error img fixed

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -7,7 +7,7 @@ const Error = () => {
     return (
       <div className="text-center my-5 py-5 d-flex flex-column">
         <div>
-          <img src={error} alt="" />
+          <img src={error} alt="" className="img-fluid" />
         </div>
         <Link to="/home">
           <Button variant="outline-warning text-dark w-25 mx-auto mt-5">
